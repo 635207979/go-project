6963fd758afaeb8f8cbb04957aa768b86c63b1d1
635207979 <635207979@qq.com>
2020-08-13 14:52:51 +0800

    init

--- src/hello/hello.go
+++ src/hello/hello.go
@@ -1,7 +1,5 @@
 package hello
 
-import "fmt"
-
 const englishHelloPrefix = "Hello, "
 
 func Hello(name string, language string) string {
@@ -13,6 +11,7 @@ func Hello(name string, language string) string {
 	}
 	return englishHelloPrefix + name
 }
-func main() {
-	fmt.Println(Hello("", ""))
-}
+
+//func main() {
+//	fmt.Println(Hello("", ""))
+//}
