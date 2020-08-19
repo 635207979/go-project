5b14ff5ed5bbbca168950942525e3df169bddccd
635207979 <635207979@qq.com>
2020-08-19 15:04:08 +0800

    添加SumAllTails

--- src/integers/sum_test.go
+++ src/integers/sum_test.go
@@ -37,3 +37,23 @@ func TestSumAll(t *testing.T) {
 		t.Errorf("got %v want %v", got, want)
 	}
 }
+
+func TestSumAllTails(t *testing.T) {
+	checkSum := func(t *testing.T, got, want []int) {
+		if !reflect.DeepEqual(got, want) {
+			t.Errorf("got %v want %v", got, want)
+		}
+	}
+	t.Run("make the sums of some slices", func(t *testing.T) {
+		got := SumAllTails([]int{1, 2}, []int{2, 9})
+		want := []int{2, 9}
+		checkSum(t, got, want)
+
+	})
+	t.Run("make the sums empty slices", func(t *testing.T) {
+		got := SumAllTails([]int{}, []int{3, 4, 5})
+		want := []int{0, 9}
+		checkSum(t, got, want)
+	})
+
+}
