a65253ae20334cf26bf57d685f4cf9cea5d47605
635207979 <635207979@qq.com>
2020-08-25 16:38:10 +0800

    添加SumAll

--- src/money/wallet.go
+++ src/money/wallet.go
@@ -1,13 +1,31 @@
 package money
 
+import (
+	"errors"
+	"fmt"
+)
+
+type Bitcoin int
+
 type Wallet struct {
-	money int
+	money Bitcoin
 }
 
-func (w *Wallet) Deposit(money int) {
+func (b Bitcoin) String() string {
+	return fmt.Sprintf("%d BTC", b)
+}
+func (w *Wallet) Deposit(money Bitcoin) {
 	w.money += money
 }
 
-func (w *Wallet) Balance() int {
+func (w *Wallet) Balance() Bitcoin {
 	return w.money
+}
+
+func (w *Wallet) Withdraw(money Bitcoin) error {
+	if w.money < money {
+		return errors.New("cannot withdraw, insufficient funds")
+	}
+	w.money -= money
+	return nil
 }
--- src/money/wallet_test.go
+++ src/money/wallet_test.go
@@ -3,15 +3,42 @@ package money
 import "testing"
 
 func TestWallet(t *testing.T) {
+	assertBalance := func(t *testing.T, wallet Wallet, want Bitcoin) {
+		got := wallet.Balance()
 
-	wallet := Wallet{}
-
-	wallet.Deposit(10)
-
-	got := wallet.Balance()
-	want := 10
-
-	if got != want {
-		t.Errorf("got %d want %d", got, want)
+		if got != want {
+			t.Errorf("got %s want %s", got, want)
+		}
 	}
+
+	assertError := func(t *testing.T, got error, want string) {
+		if got == nil {
+			t.Fatal("didn't get an error but wanted one")
+		}
+
+		if got.Error() != want {
+			t.Errorf("got '%s', want '%s'", got, want)
+		}
+	}
+
+	t.Run("Deposit", func(t *testing.T) {
+		wallet := Wallet{}
+		wallet.Deposit(Bitcoin(10))
+		want := Bitcoin(10)
+		assertBalance(t, wallet, want)
+	})
+	t.Run("Withdraw", func(t *testing.T) {
+		wallet := Wallet{money: Bitcoin(10)}
+		wallet.Withdraw(Bitcoin(10))
+		want := Bitcoin(0)
+		assertBalance(t, wallet, want)
+	})
+	t.Run("withdraw more than balance", func(t *testing.T) {
+		wallet := Wallet{money: Bitcoin(20)}
+		err := wallet.Withdraw(Bitcoin(20))
+		want := Bitcoin(10)
+		assertBalance(t, wallet, want)
+		assertError(t, err, "cannot withdraw, insufficient funds")
+	})
+
 }
